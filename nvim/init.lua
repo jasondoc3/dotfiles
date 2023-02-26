c76c8bc88fc41ed41cb32e6be77f7f1a5310ce78
Jason Dougherty <jasondoc3@gmail.com>
2023-02-25 16:32:17 -0800

    Add Ctrl-T for live_grep

--- nvim/init.lua
+++ nvim/init.lua
@@ -92,7 +92,7 @@ require('telescope').setup{
   }
 }
 vim.keymap.set('n', '<C-p>', telescope.find_files, {})
-vim.keymap.set('n', 'fg', telescope.live_grep, {})
+vim.keymap.set('n', '<C-t>', telescope.live_grep, {})
 vim.keymap.set('n', 'fb', telescope.buffers, {})
 vim.keymap.set('n', 'fh', telescope.help_tags, {})
 
