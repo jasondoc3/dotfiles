cec3522173cb327a91ce65557b0c0e62c7b3a1f0
Jason Dougherty <jasondoc3@gmail.com>
2023-03-22 21:00:54 -0700

    Fix indentation for now

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -12,16 +12,19 @@ return {
 				autotag = { enable = true },
 				ensure_installed = { "c", "lua", "vim", "help", "markdown", "markdown_inline" },
 				auto_install = true,
-				indent = {
-					enable = true,
-				},
+				-- Indentation is wonky for ruby and golang. Disabling for now, relying on set autoindent
+				-- Note that additional_vim_regex_highlighting needs to be true
+				--
+				-- indent = {
+				--		enable = true,
+				--		},
 				highlight = {
 					enable = true,
 					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 					-- Using this option may slow down your editor, and you may see some duplicate highlights.
 					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
+					additional_vim_regex_highlighting = true,
 				},
 			})
 		end,
