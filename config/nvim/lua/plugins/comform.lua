0d6bb51277a977c43a83bfe92ddb14f3949480ac
Jason Dougherty <jasondoc3@gmail.com>
2024-04-03 14:09:34 -0700

    Add jq formatting

--- config/nvim/lua/plugins/comform.lua
+++ config/nvim/lua/plugins/comform.lua
@@ -6,6 +6,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = { "gofmt", "goimports" },
+				json = { "jq" },
 			},
 			format_on_save = {
 				-- These options will be passed to conform.format()
