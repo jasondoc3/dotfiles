a2f66f9ebe4804b72e81a89d2ebb8a4625f0f360
Jason Dougherty <jasondoc3@gmail.com>
2024-06-17 21:28:38 -0700

    Lualine displays relative path

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,6 +1,21 @@
 return {
 	"folke/tokyonight.nvim",
-	"nvim-lualine/lualine.nvim",
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				sections = {
+					lualine_c = {
+						{
+							"filename",
+							file_status = true, -- displays file status (readonly status, modified status)
+							path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+						},
+					},
+				},
+			})
+		end,
+	},
 	{
 		"windwp/nvim-autopairs",
 		config = function()
