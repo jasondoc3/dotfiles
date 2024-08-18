3a19cc35c6dc27657209a8f84c74eeccaf85bfa8
Jason Dougherty <jasondoc3@gmail.com>
2024-08-18 15:58:16 -0700

    Fix install script

--- install.sh
+++ install.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 
-RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
-NC='\033[0m'
+NC='\033[0m' # No Color
 
-echo "Installing dotfiles."
-echo "Creating .config directory if it doesn't exist."
+# Get the path of the script
+# Change to the script's directory and suppress output
+pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
+
+# Capture the current directory as the script directory
+SCRIPT_DIR=$(pwd)
+
+# Return to the original directory and suppress output
+popd > /dev/null
+
+echo "Installing dotfiles..."
 mkdir -p $HOME/.config
+echo -e "Created ${BLUE}\"$HOME/.config\"${NC} directory if it did not exist"
 
 for config in "nvim" "zellij" "kitty"; do
-  ln -sf $PWD/$config $HOME/.config/$config
+  ln -sf $SCRIPT_DIR/$config $HOME/.config/$config
   echo -e "${BLUE}Created symlink for $config"
 done
 
 for file in "zshrc" "gitconfig"; do
-  ln -sf $PWD/$file $HOME/.$file
+  ln -sf $SCRIPT_DIR/$file $HOME/.$file
   echo -e "${BLUE}created symlink for .$file"
 done
 
