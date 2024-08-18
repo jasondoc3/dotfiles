6a442acdb9efc37d603ae76be13626bfb1a79581
Jason Dougherty <jasondoc3@gmail.com>
2024-08-18 16:25:00 -0700

    DRY install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
@@ -14,19 +15,29 @@ SCRIPT_DIR=$(pwd)
 # Return to the original directory and suppress output
 popd > /dev/null
 
+create_symlink() {
+  local path="$1"
+  local target="$2"
+
+  if [ -e "$target" ]; then
+    echo -e "Removing existing ${RED}$target${NC}"
+    rm -rf "$target"
+  fi
+
+  ln -s $path $target
+  echo -e "Created symlink ${BLUE}$path -> $target${NC}"
+}
+
 echo "Installing dotfiles..."
 mkdir -p $HOME/.config
 echo -e "Created ${BLUE}\"$HOME/.config\"${NC} directory if it did not exist"
 
 for config in "nvim" "zellij" "kitty"; do
-  ln -sf $SCRIPT_DIR/$config $HOME/.config/$config
-  echo -e "${BLUE}Created symlink for $config"
+  create_symlink "$SCRIPT_DIR/$config" "$HOME/.config/$config"
 done
 
 for file in "zshrc" "gitconfig"; do
-  ln -sf $SCRIPT_DIR/$file $HOME/.$file
-  echo -e "${BLUE}created symlink for .$file"
+  create_symlink "$SCRIPT_DIR/$file" "$HOME/.$file"
 done
 
-GREEN='\033[0;32m'
-echo -e "${GREEN}All done"
+echo -e "${GREEN}All done${NC}"
