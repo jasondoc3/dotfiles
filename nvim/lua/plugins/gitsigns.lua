8a9f4dfa737ddeac6783a146e04eab472d6ed601
Jason Dougherty <jasondoc3@gmail.com>
2024-09-29 16:15:46 -0700

    Use nvim scrollbar

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,12 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	dependencies = {
+		"petertriho/nvim-scrollbar",
+	},
 	config = function()
 		require("gitsigns").setup({
 			current_line_blame = true,
 		})
+		require("scrollbar.handlers.gitsigns").setup()
 	end,
 }
