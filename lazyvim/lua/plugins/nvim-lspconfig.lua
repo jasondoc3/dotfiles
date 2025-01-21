58ea3e2f00592aed7de6d7b727c3f675a256f4d1
Jason Dougherty <jasondoc3@gmail.com>
2025-01-20 19:18:21 -0800

    Use vtsls

--- lazyvim/lua/plugins/nvim-lspconfig.lua
+++ lazyvim/lua/plugins/nvim-lspconfig.lua
@@ -1,34 +1,29 @@
 return {
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        rubocop = {
-          mason = false,
-          cmd = { "bundle", "exec", "rubocop", "--lsp" },
-          init_options = {
-            safeAutocorrect = false,
-          },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- Use rubocop for formatting and diagnostics
+      rubocop = {
+        mason = false,
+        cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        init_options = {
+          safeAutocorrect = false,
         },
-        -- Only use solagraph for autocompletion
-        solargraph = {
-          mason = false,
-          cmd = { "bundle", "exec", "solargraph", "stdio" },
-          init_options = {
-            formatting = false,
-          },
-          settings = {
-            solargraph = {
-              diagnostics = false,
-            },
+      },
+      -- Only use solagraph for autocompletion
+      solargraph = {
+        mason = false,
+        cmd = { "bundle", "exec", "solargraph", "stdio" },
+        init_options = {
+          formatting = false,
+        },
+        settings = {
+          solargraph = {
+            diagnostics = false,
           },
         },
       },
+      vtsls = {},
     },
   },
 }
