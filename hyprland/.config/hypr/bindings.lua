eec4fad9f730d7a8b57dd89ed00b7e3aa0c0442e
Jason Dougherty <jasondoc3@gmail.com>
2026-08-31 11:38:37 -0700

    Unbind google maps hyprland key

--- hyprland/.config/hypr/bindings.lua
+++ hyprland/.config/hypr/bindings.lua
@@ -24,6 +24,7 @@
 -- hl.unbind("SUPER + SHIFT + B")
 
 -- Logitech MX Keys examples:
+hl.unbind("SUPER + SHIFT + S") -- Formerly google maps
 o.bind("SUPER + SHIFT + S", nil, "omarchy-capture-screenshot")
 -- o.bind("SUPER + H", nil, "voxtype record toggle")
 -- o.bind("SUPER + PERIOD", nil, "omarchy-shell shell toggle omarchy.emojis")
